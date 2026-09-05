a97f26b56777d0e9c292fc5a977d93501ce2f55b
SplitSky <24545133+SpitSky@users.noreply.github.com>
2026-09-05 22:43:09 +0100

    feat: first skeleton of TPC server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
+
+fn main() -> std::io::Result<()> {
+    // bind to locahost
+    let listener = TcpListener::bind("127.0.0.1:8080").expect("Failed to bind");
+
+    println!("Server is listening on 127.0.0.1:8080");
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(mut stream) => {
+                println!("New connection: {}", stream.peer_addr()?);
+
+                // read data from client
+                let mut buffer = [0; 512];
+                match stream.read(&mut buffer) {
+                    Ok(n) => {
+                        println!("Received: {}", String::from_utf8_lossy(&buffer[..n]));
+                        let response = b"Hello client!";
+                        stream.write_all(response).expect("Failed to write");
+                        stream.flush().expect("Failed to flush");
+                    }
+                    Err(e) => println!("Error read: {}", e),
+                }
+            }
+            Err(e) => println!("Error accepting: {}", e),
+        }
+    }
+    Ok(())
 }
